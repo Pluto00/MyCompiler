390fd412819bae8f02b90cc5e1eba065e11a46e3
Pluto00 <1834733586@qq.com>
2021-05-31 18:42:01 +0800

    feat: add token elements definition

---
+++ Lexer/Tokens.py
@@ -0,0 +1,119 @@
+from enum import Enum
+
+
+class LexicalElement:
+    def get(self):
+        return self.value
+
+    def __repr__(self):
+        return f"<{self.value}>"
+
+    def describe(self):
+        return self.__repr__()
+
+
+class KeyWordType(LexicalElement, Enum):
+    Do = "do"
+    While = "while"
+    Switch = "switch"
+    Case = "case"
+    If = "if"
+    Then = "then"
+    Else = "else"
+    Until = "until"
+    For = "for"
+    Continue = "continue"
+    Break = "break"
+    Goto = "goto"
+
+
+class NonWordType(LexicalElement, Enum):
+    Addition = "+"
+    Increment = "++"
+    Subtraction = "-"
+    Multiplication = "*"
+    Division = "/"
+    Modulus = "%"
+    Decrement = "--"
+    Equal = "="
+    NotEqual = "<>"
+    AddEqual = "+="
+    SubEqual = "-="
+    MulEqual = "*="
+    DivEqual = "/="
+    ModEqual = "%="
+    ShiftLeft = "<<"
+    ShiftRight = ">>"
+    Less = "<"
+    Greater = ">"
+    LE = "<="
+    GE = ">="
+    LeftSquareBracket = "["
+    RightSquareBracket = "]"
+    Dot = "."
+    Comma = ","
+    Assign = ":="
+    Colon = ":"
+    Semicolon = ";"
+    LeftParentheses = "("
+    RightParentheses = ")"
+    LogicalAND = "&&"
+    LogicalOr = "||"
+    LogicalNot = "!"
+    LogicalNotEqual = "!="
+    BinaryXor = "^"
+    BinaryAND = "&"
+    BinaryOr = "|"
+    LeftCurlyBracket = "{"
+    RightCurlyBracket = "}"
+
+
+class LiteralElement(LexicalElement):
+    def __repr__(self):
+        return f"<{self.name}, {self.value}>"
+
+
+class IntegerLiteral(LiteralElement):
+    def __init__(self, val):
+        assert type(val) == int
+        self.name = "integer"
+        self.value = val
+
+
+class FloatLiteral(LiteralElement):
+    def __init__(self, val):
+        assert type(val) == float
+        self.name = "float"
+        self.value = val
+
+
+class StringLiteral(LiteralElement):
+    def __init__(self, val):
+        assert type(val) == str
+        self.name = "string"
+        self.value = val
+
+
+class IdentifierElement(LexicalElement):
+    def __init__(self, val):
+        self.value = val
+
+    def __repr__(self):
+        return f"<{self.value}>"
+
+
+class LineFeedElement(LexicalElement):
+    def __init__(self):
+        self.value = "\n"
+
+    def __repr__(self):
+        return '\n'
+
+
+class ErrorElement(LexicalElement):
+    def __init__(self, lineno, reason):
+        self.lineno = lineno
+        self.reason = reason
+
+    def __repr__(self):
+        return "error line %d: %s" % (self.lineno, self.reason)
