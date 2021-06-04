976d2866f5f626a1549086a6171ffb121dbb2ede
Pluto00 <1834733586@qq.com>
2021-06-04 11:37:21 +0800

    feat: update web ui

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 from Lexer import Lexer
+from SyntaxAnalyzer import SLRTable
 
 app = Flask(__name__)
 lex = Lexer()
+slr = SLRTable(lexer=lex)
 
 
 @app.route("/")
@@ -17,6 +19,21 @@ def post_lexer_code():
         fh.write(code)
     with open('code.cache', encoding='utf-8') as fh:
         response = lex.parse(fh)
+    return jsonify(response)
+
+
+@app.route("/api/syntax/analyzer/code", methods=["POST"])
+def post_syntax_analyzer_code():
+    code = request.json.get("code")
+    with open('code.cache', 'w', encoding='utf-8') as fh:
+        fh.write(code)
+    with open('code.cache', encoding='utf-8') as fh:
+        res = slr.analyze(fh)
+    response = {"output": ""}
+    response["output"] += "栈顶     输入     操作     匹配状态\n"
+    for i in range(len(res['stack'])):
+        response["output"] += ("%-8s %-8s %-8s " % (
+            res['stack'][i], res['input'][i], res['operations'][i])) + ' '.join(res['matched'][i + 1]) + '\n'
     return jsonify(response)
 
 
