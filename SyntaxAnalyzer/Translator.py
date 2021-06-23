908bbbd01ecec4d8b194996f71513419c9510c36
Pluto00 <1834733586@qq.com>
2021-06-24 01:40:15 +0800

    feat: update grammar define file

--- SyntaxAnalyzer/Translator.py
+++ SyntaxAnalyzer/Translator.py
@@ -1,17 +1,17 @@
 class Attribute:
-    def __init__(self, type="", value="", addr="null", trueList=None, falseList=None, instr=-1,
-                 nextList=None, arrType=0, preList=None):
-        if preList is None:
-            preList = []
+    def __init__(self, type="", value="", addr="null", true_list=None, false_list=None, instr=-1,
+                 next_list=None, array_depth=0, pre_list=None):
+        if pre_list is None:
+            pre_list = []
         self.type = type
         self.value = value
         self.addr = addr
-        self.trueList = trueList
-        self.falseList = falseList
+        self.true_list = true_list
+        self.false_list = false_list
         self.instr = instr
-        self.nextList = nextList
-        self.arrType = arrType
-        self.preList = preList
+        self.next_list = next_list
+        self.array_depth = array_depth
+        self.pre_list = pre_list
 
 
 class Code:
@@ -38,42 +38,44 @@ class Translator:
     def reduce_process(self, b, p):
         left = p.head
         right = ' '.join(p.body)
-        eval("self.action{}(left, right)".format(b))
+        getattr(self, 'action' + str(b))(left, right)
+        # eval("self.action{}(left, right)".format(b))
 
-    def action0(left, right):
+    def action0(self, left, right):
+        # S' -> S
         pass
 
     def action1(self, left, right):
-        # S -> program id stmts
+        # S -> program id S
         right = len(right.split(" "))
         for i in range(right):
             self.attributes.pop()
         self.attributes.append(Attribute(left, "null", "null"))
 
     def action2(self, left, right):
-        # stmts -> { stmt M }
+        # S -> { L M }
         self.attributes.pop()
-        M = self.attributes.pop().instr
-        stmts = self.attributes.pop().nextList
+        M = self.attributes.pop()
+        stmts = self.attributes.pop()
         self.attributes.pop()
-        self.backpatch(stmts, M)
+        self.back_patch(stmts.next_list, M.instr)
         self.attributes.append(Attribute(left, "null", "null"))
 
     def action3(self, left, right):
-        # stmt -> stmts
-        stmts = self.attributes.pop().nextList
-        self.attributes.append(Attribute(left, "null", "null", nextList=stmts))
+        # L -> S
+        stmts = self.attributes.pop().next_list
+        self.attributes.append(Attribute(left, "null", "null", next_list=stmts))
 
     def action4(self, left, right):
-        # stmt -> stmt M stmts
-        stmts = self.attributes.pop().nextList
-        M = self.attributes.pop().instr
-        stmt = self.attributes.pop().nextList
-        self.backpatch(stmt, M)
-        self.attributes.append(Attribute(left, "null", "null", nextList=stmts))
+        # L -> L M S
+        stmts = self.attributes.pop()
+        M = self.attributes.pop()
+        stmt = self.attributes.pop()
+        self.back_patch(stmt.next_list, M.instr)
+        self.attributes.append(Attribute(left, "null", "null", next_list=stmts.next_list))
 
     def action5(self, left, right):
-        # stmts -> id = expr ;
+        # S -> id =|:= E ;
         self.attributes.pop()
         expr = self.attributes.pop().addr
         self.attributes.pop()
@@ -82,21 +84,21 @@ class Translator:
         self.codes.append(Code("=", expr, "null", ids))
 
     def action6(self, left, right):
-        # stmts -> id := expr ;
         self.action5(left, right)
 
     def action7(self, left, right):
-        # stmts -> L = expr ;
+        # S -> Arr = E ;
         self.attributes.pop()
         expr = self.attributes.pop().addr
         self.attributes.pop()
-        ids = self.attributes[-1].addr
-        array = self.attributes.pop().value
-        self.attributes.append(Attribute(left, "null", ids))
-        self.codes.append(Code("[]=", ids, expr, array))
+        L = self.attributes.pop()
+        addr = L.addr  # 数组的地址
+        array = L.value  # 数组的标识符
+        self.attributes.append(Attribute(left, "null", addr))
+        self.codes.append(Code("[]=", addr, expr, array))
 
     def action8(self, left, right):
-        # stmts -> id += expr ;
+        # S -> id +=|-=|*=|/= E ;
         self.attributes.pop()
         expr = self.attributes.pop().addr
         op = self.attributes.pop().type
@@ -105,19 +107,16 @@ class Translator:
         self.codes.append(Code(op, ids, expr, ids))
 
     def action9(self, left, right):
-        # stmts -> id -= expr
         self.action8(left, right)
 
     def action10(self, left, right):
-        # stmts -> id *= expr
         self.action8(left, right)
 
     def action11(self, left, right):
-        # stmts -> id /= expr
         self.action8(left, right)
 
     def action12(self, left, right):
-        # stmts -> if ( bool )  M stmts N else M stmts
+        # S -> if ( B ) M S N else M S
         stmt2 = self.attributes.pop()
         M2 = self.attributes.pop()
         self.attributes.pop()
@@ -128,78 +127,76 @@ class Translator:
         B = self.attributes[-1]
         self.attributes.pop()
         self.attributes.pop()
-        self.backpatch(B.trueList, M1.instr)
-        self.backpatch(B.falseList, M2.instr)
-        nextList = self.merge(stmt1.nextList, N.nextList)
-        nextList = self.merge(nextList, stmt2.nextList)
-        self.attributes.append(Attribute(left, "null", "null", None, None, -1, nextList))
+        self.back_patch(B.true_list, M1.instr)
+        self.back_patch(B.false_list, M2.instr)
+        next_list = self.merge(stmt1.next_list, N.next_list)
+        next_list = self.merge(next_list, stmt2.next_list)
+        self.attributes.append(Attribute(left, "null", "null", None, None, -1, next_list))
 
     def action13(self, left, right):
-        # stmts -> if ( bool ) M stmts
+        # S -> if ( B ) M S
         stmt1 = self.attributes.pop()
         M = self.attributes.pop()
         self.attributes.pop()
         B = self.attributes.pop()
         self.attributes.pop()
         self.attributes.pop()
-        self.backpatch(B.trueList, M.instr)
-        nextList = self.merge(B.falseList, stmt1.nextList)
-        self.attributes.append(Attribute(left, "null", "null", None, None, -1, nextList))
+        self.back_patch(B.true_list, M.instr)
+        next_list = self.merge(B.false_list, stmt1.next_list)
+        self.attributes.append(Attribute(left, "null", "null", None, None, -1, next_list))
 
     def action14(self, left, right):
-        # while_stmt -> while M ( bool ) M stmts
-        stmt = self.attributes.pop().nextList
+        # L -> while M ( B ) M S
+        stmt = self.attributes.pop().next_list
         M2 = self.attributes.pop().instr
         self.attributes.pop()
-        trueList = self.attributes[-1].trueList
-        falseList = self.attributes.pop().falseList
+        true_list = self.attributes[-1].true_list
+        false_list = self.attributes.pop().false_list
         self.attributes.pop()
         M1 = self.attributes.pop().instr
         self.attributes.pop()
-        self.backpatch(stmt, M1)
-        self.backpatch(trueList, M2)
-        self.attributes.append(Attribute(left, "null", "null", None, None, -1, falseList))
+        self.back_patch(stmt, M1)
+        self.back_patch(true_list, M2)
+        self.attributes.append(Attribute(left, "null", "null", None, None, -1, false_list))
         self.codes.append(Code("goto", "null", "null", str(M1 + 100)))
 
     def action15(self, left, right):
-        # bool -> bool || M bool
+        # B -> B || M B
         B2 = self.attributes.pop()
         M = self.attributes.pop()
         self.attributes.pop()
         B1 = self.attributes.pop()
-        self.backpatch(B1.falseList, M.instr)
-        trueList = self.merge(B1.trueList, B2.trueList)
-        self.attributes.append(Attribute(left, "null", "null", trueList, B2.falseList, -1))
+        self.back_patch(B1.false_list, M.instr)
+        true_list = self.merge(B1.true_list, B2.true_list)
+        self.attributes.append(Attribute(left, "null", "null", true_list, B2.false_list, -1))
 
     def action16(self, left, right):
-        # bool -> bool && M bool
+        # B -> B && M B
         B2 = self.attributes.pop()
         M = self.attributes.pop()
         self.attributes.pop()
         B1 = self.attributes.pop()
-        self.backpatch(B1.trueList, M.instr)
-        falseList = self.merge(B1.falseList, B2.falseList)
-        self.attributes.append(Attribute(left, "null", "null", B2.trueList, falseList, -1))
+        self.back_patch(B1.true_list, M.instr)
+        false_list = self.merge(B1.false_list, B2.false_list)
+        self.attributes.append(Attribute(left, "null", "null", B2.true_list, false_list, -1))
 
     def action17(self, left, right):
-        # bool -> ! bool
+        # B -> ! B
         B1 = self.attributes.pop()
-        trueList = B1.falseList
-        falseList = B1.trueList
-        self.attributes.append(Attribute(left, "null", "null", trueList, falseList, -1))
+        true_list = B1.false_list
+        false_list = B1.true_list
+        self.attributes.append(Attribute(left, "null", "null", true_list, false_list, -1))
 
     def action18(self, left, right):
-        # bool -> expr NE|LEQ|<|LE|>|GE expr```
+        # B -> E !=|==|<=|<|>|>= E
         expr2 = self.attributes.pop().addr
         op = self.attributes.pop().type
         expr1 = self.attributes.pop().addr
-        trueList = []
-        trueList.append(len(self.codes))
-        falseList = []
-        falseList.append(len(self.codes) + 1)
-        self.attributes.append(Attribute(left, "null", "null", trueList, falseList))
-        self.codes.append(Code(op, expr1, expr2, "_"))
-        self.codes.append(Code("goto", "null", "null", "_"))
+        true_list = [len(self.codes)]
+        false_list = [len(self.codes) + 1]
+        self.attributes.append(Attribute(left, "null", "null", true_list, false_list))
+        self.codes.append(Code(op, expr1, expr2, "_"))  # true_list: if ( expr1 REL expr2 ) goto _
+        self.codes.append(Code("goto", "null", "null", "_"))  # false_list: goto _
 
     def action19(self, left, right):
         self.action18(left, right)
@@ -217,7 +214,7 @@ class Translator:
         self.action18(left, right)
 
     def action24(self, left, right):
-        # expr -> expr +|-|*|/|êxpr
+        # E -> E +|-|*|/|^ E
         factor = self.attributes.pop().addr
         op = self.attributes.pop().type
         term1 = self.attributes.pop().addr
@@ -238,102 +235,90 @@ class Translator:
         self.action24(left, right)
 
     def action29(self, left, right):
-        # expr -> factor
+        # E -> F
         tmp = self.attributes[-1].addr
         self.attributes.pop()
         self.attributes.append(Attribute(left, "null", tmp))
 
     def action30(self, left, right):
-        # factor -> id | number
+        # F -> id
         tmp = self.attributes[-1].value
         self.attributes.pop()
         self.attributes.append(Attribute(left, "null", tmp))
 
     def action31(self, left, right):
-        # factor -> id | number
+        # F -> Num
         self.action30(left, right)
 
     def action32(self, left, right):
-        # factor -> ( expr )
+        # F -> ( E )
         self.attributes.pop()
         expr = self.attributes.pop().addr
         self.attributes.pop()
         self.attributes.append(Attribute(left, "null", expr))
 
     def action33(self, left, right):
-        # M -> <empty>
-        self.attributes.append(Attribute(left, "null", "null", None, None, len(self.codes)))
+        # M -> ε
+        # 生成下一条指令的序号，放在综合属性 instr 中
+        self.attributes.append(Attribute(left, "null", instr=len(self.codes)))
 
     def action34(self, left, right):
-        # N -> <empty>
-        nextList = []
-        nextList.append(len(self.codes))
-        self.attributes.append(Attribute(left, "null", "null", None, None, -1, nextList))
+        # N -> ε
+        next_list = [len(self.codes)]
+        self.attributes.append(Attribute(left, "null", next_list=next_list))
         self.codes.append(Code("goto", "null", "null", "_"))
 
     def action35(self, left, right):
-        # expr -> L
+        # E -> Arr
         term = self.get_temp()
-        L = self.attributes[-1].addr
-        ids = self.attributes.pop().value
+        addr = self.attributes[-1].addr  # 数组的地址
+        ids = self.attributes.pop().value  # 数组的标识符
         self.attributes.append(Attribute(left, ids, term))
-        self.codes.append(Code("=[]", ids, L, term))
+        self.codes.append(Code("=[]", ids, addr, term))  # term = ids[addr]
 
     def action36(self, left, right):
-        # L -> id [ expr ]
+        # Arr -> id [ E ]
         self.attributes.pop()
         expr = self.attributes.pop()
         self.attributes.pop()
         ids = self.attributes.pop()
         addr = self.get_temp()
         code = Code("*", expr.addr, 4, addr)
-        preList = []
-        preList.append(code)
-        self.attributes.append(Attribute(left, ids, addr, arrType=1, preList=preList))
+        pre_list = [code]
+        self.attributes.append(Attribute(left, ids, addr, array_depth=1, pre_list=pre_list))
         self.codes.append(code)
 
     def action37(self, left, right):
-        # L -> L [ expr ]
+        # Arr -> Arr [ E ]
         self.attributes.pop()
         expr = self.attributes.pop()
         self.attributes.pop()
         L1 = self.attributes.pop()
         t = self.get_temp()
         addr = self.get_temp()
-        arrType = -1
         code = Code("*", expr.addr, 4, t)
-        preList = []
-        if L1.arrType == 1:
-            preList = L1.preList
-            preList[0].arg2 = 80
-            preList.append(code)
-            arrType = 2
-        elif L1.arrType == 2:
-            arrType = 3
-            preList = L1.preList
-            preList[0].arg2 = 2400
-            preList[1].arg2 = 120
-            preList.append(code)
-
-        else:
-            arrType = 4
-            preList = L1.preList
-            if not self.hasError:
-                # err = error('不支持更高维的数组')
-                hasError = True
-        sym = Attribute(left, L1.value, addr, arrType=arrType, preList=preList)
-        # print(sym.arrType)
-        self.attributes.append(sym)
+        default_len = [i * 10 for i in range(1, 9)]  # 默认数组维度 int [10][20][30][40][50]...
+        current_width = default_len[L1.array_depth]
+        # l * 4
+        # k * 4 * 40
+        # j * 4 * 30 * 40
+        # i * 4 * 20 * 30 * 40
+        for pl in L1.pre_list:
+            pl.arg2 *= current_width
+        pre_list = L1.pre_list + [code]
+        self.attributes.append(Attribute(left, L1.value, addr, array_depth=L1.array_depth + 1, pre_list=pre_list))
         self.codes.append(code)
         self.codes.append(Code("+", L1.addr, t, addr))
 
     def action38(self, left, right):
+        # Num -> integer
         pass
 
     def action39(self, left, right):
+        # Num -> float
         pass
 
-    def backpatch(self, lists, m):
+    def back_patch(self, lists, m):
         if lists is not None and len(lists) != 0:
             for i in lists:
                 self.codes[i].result = int(m + 100)
